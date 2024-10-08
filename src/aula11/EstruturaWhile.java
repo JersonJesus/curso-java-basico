f1ef5bcab3ed3e2e3b82acc5e78e8f7129ffc03e
Jerson André <alkimistakillas@gmail.com>
2024-10-08 17:41:32 +0100

    Aula 11

---
+++ src/aula11/EstruturaWhile.java
@@ -0,0 +1,25 @@
+package aula11;
+
+public class EstruturaWhile {
+ 
+    public static void main(String[] args) {
+        
+        int c = 0;
+        
+        while (c < 20) {  
+            
+            c++;
+            
+            if (c == 5 || c == 10) {
+                continue;
+            } else if(c == 15) {
+                break;
+            }
+            
+            
+            System.out.println(c + "º Cambalhota");
+            
+            
+        }
+    }
+}
